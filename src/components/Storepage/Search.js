664051a4142c5a0c4df20a1497d963cca36af4aa
kevin-magu <wanjaukevinmagu@gmail.com>
2024-04-16 00:57:25 +0300

    web store

--- src/components/Storepage/Search.js
+++ src/components/Storepage/Search.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Search() {
   return (
     <div className='search-container'>
-        <input type="search" /> <select>
+        <input type="search" /> 
+        <select className="filter">
+          <option value="" disabled selected>Filter by:</option>
           <option value="Lenovo" key="">Lenovo</option>
           <option value="HP" key="">HP</option>
           <option value="Mac" key="">Mac</option>
