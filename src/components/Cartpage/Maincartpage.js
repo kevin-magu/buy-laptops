93ecd6d7f36cd6983c34fd9e09cdc3c7f9228b17
kevin-magu <wanjaukevinmagu@gmail.com>
2024-06-01 06:22:42 +0300

    real-estate

--- src/components/Cartpage/Maincartpage.js
+++ src/components/Cartpage/Maincartpage.js
@@ -4,7 +4,15 @@ function Maincartpage() {
   return (
     <div className='cart-main-page-wrapper'>
         <h4 className='cart-heading'>Cart</h4>
-        <div className='items-main-section'></div>
+        <div className='items-main-section'>
+            <h3 className='item-category'>Laptops</h3>
+            <div className='item-main-section'>
+                <div className='item-image'></div>
+                <div className='item-quantity'></div>
+                <div className='item-price'></div>
+                <div className='item-total-price'></div>
+            </div>
+        </div>
     </div>
   )
 }
