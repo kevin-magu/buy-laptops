d5e3e56c2bece7ed47c41b96a1229916209a4288
kevin-magu <wanjaukevinmagu@gmail.com>
2024-06-06 14:55:38 +0300

    real-estate

--- src/components/Cartpage/Maincartpage.js
+++ src/components/Cartpage/Maincartpage.js
@@ -1,22 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { db, storage } from '../../Firebaseconfig'
 import { collection, getDocs } from 'firebase/firestore'
-
+import { auth } from '../../Firebaseconfig'
+import userEvent from '@testing-library/user-event'
 function Maincartpage() {
 /* 
   loop through storage and get the image of the laptop with the same ID
 */
-const currentLoggedInUser = async () => {
+    const [userEmail,setUserEmail] = useState('')
+    const [cartItems, setCartItems] = useState('')
+    const [iteamId, setItemsId] = useState([])
+
+    useEffect (() =>{
+      const user = auth.currentUser
+      if(user){
+        setUserEmail(user.email)
+      }
+    }, [])
+    
+    useEffect(() =>{
+        const fetchItemsfromCart = async () =>{
+          if(userEmail){
+          try{
+            const itemsCollection = collection(db, `cart/${userEmail}/items`)
+            const itemsSnapshot = await getDocs(itemsCollection);
+            const itemsList = itemsSnapshot.docs.map(doc => doc.data())
+            setCartItems(itemsList)
+            console.log("items from database",cartItems)
+
+          }catch(error){
+            console.error("error fetching cart items", error) 
+          }
+      }
+      
+    }
+    fetchItemsfromCart()
+    }, [userEmail])
   
-}
-const getCartItemsImages = async () => {
-  const cartItemsIds= []
-  const itemsFromFirestore = await getDocs(collection(db, "carts/items"))
-  console.log("items from firestore",itemsFromFirestore)
-}
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+    console.log("this is the user email",userEmail)
+
+    const getCartItemsImages = async () => {
+    const cartItemsIds= []
+    
+    
+    
+} 
 
 getCartItemsImages()
-
   return (
     <div className='cart-main-page-wrapper'>
         <h4 className='cart-heading'>Cart</h4>
